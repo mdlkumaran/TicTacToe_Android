82bfa5ceafddb3f882e5f143be761223cede1c77
mdlkumaran <mdlkumaran@gmail.com>
2016-09-15 19:51:21 -0400

    Added animation functionality

--- app/src/main/java/com/kumaran/tictactoe_android/MainActivity.java
+++ app/src/main/java/com/kumaran/tictactoe_android/MainActivity.java
@@ -6,13 +6,21 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //0 =yellow, 1 = red
+    int activePlayer = 0;
     public void dropIn(View view)
         {
             ImageView counter = (ImageView) view;
             counter.setTranslationY(-1000f);
-            counter.setImageResource(R.drawable.yellow);
-            counter.animate().translationYBy(1000f).setDuration(300);
+            if(activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            }
+            else{
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1000f).rotation(1800).setDuration(300);
         }
 
     @Override
